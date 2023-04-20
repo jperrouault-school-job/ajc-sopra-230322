82568dc7a3b32e53ec687e03917b9437cf813538
Jeremy <jeremy.perrouault@ascadis.fr>
2023-04-20 10:48:24 +0200

    JOUR 1 - EMF & EM

--- TP/JPA-HIBERNATE/eshop-jpa-correction/src/main/java/fr/formation/Application.java
+++ TP/JPA-HIBERNATE/eshop-jpa-correction/src/main/java/fr/formation/Application.java
@@ -1,10 +1,16 @@
 package fr.formation;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Création de l'EntityManagerFactory
+		// Notre unité de persistance s'appelle EShopUnit
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("EShopUnit");
+		
+		// Une fois qu'on a l'EMF, on peut créer un EntityManager
+		EntityManager em = emf.createEntityManager();
 	}
-
 }
