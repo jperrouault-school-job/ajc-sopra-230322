7cc1c69299b2fe6acaa30b7f8551d97de31873ff
Jeremy <jeremy.perrouault@ascadis.fr>
2023-04-20 12:45:13 +0200

    JOUR 1 - Mapping Produit

--- TP/JPA-HIBERNATE/eshop-jpa-correction/src/main/java/fr/formation/Application.java
+++ TP/JPA-HIBERNATE/eshop-jpa-correction/src/main/java/fr/formation/Application.java
@@ -1,5 +1,8 @@
 package fr.formation;
 
+import java.util.List;
+
+import fr.formation.model.Produit;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -12,5 +15,16 @@ public class Application {
 		
 		// Une fois qu'on a l'EMF, on peut créer un EntityManager
 		EntityManager em = emf.createEntityManager();
+		
+		
+		// Récupération de la liste des produits
+		List<Produit> produits = em
+			.createQuery("select p from Produit p", Produit.class)
+			.getResultList();
+		
+		// Parcours
+		for (Produit p : produits) {
+			System.out.println(p.getNom());
+		}
 	}
 }
