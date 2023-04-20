65c9a2b63905ff6135cbfb577bfae16a0c2fc232
Jeremy <jeremy.perrouault@ascadis.fr>
2023-04-20 09:19:47 +0200

    JOUR 1 - Exemples MAVEN

---
+++ TP/MAVEN/eshop-java-formation-maven/src/main/java/fr/formation/Application.java
@@ -0,0 +1,9 @@
+package fr.formation;
+
+import fr.formation.model.Produit;
+
+public class Application {
+	public static void main(String[] args) {
+		Produit produit = new Produit();
+	}
+}
---
+++ TP/MAVEN/formation-maven-demo/src/main/java/fr/formation/DemoSpring.java
@@ -0,0 +1,8 @@
+package fr.formation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoSpring {
+
+}
