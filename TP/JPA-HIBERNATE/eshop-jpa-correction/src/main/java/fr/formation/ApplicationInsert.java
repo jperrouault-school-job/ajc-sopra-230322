0c967a875f19e2ea9324de6287950490e4136593
Jeremy <jeremy.perrouault@ascadis.fr>
2023-04-21 12:56:03 +0200

    JOUR 2 - UPDATE & DELETE

--- TP/JPA-HIBERNATE/eshop-jpa-correction/src/main/java/fr/formation/ApplicationInsert.java
+++ TP/JPA-HIBERNATE/eshop-jpa-correction/src/main/java/fr/formation/ApplicationInsert.java
@@ -28,12 +28,60 @@ public class ApplicationInsert {
 		em.getTransaction().begin(); // On démarre la transaction
 		
 		try {
-			em.persist(nouveauFournisseur);	
+			em.persist(nouveauFournisseur);
 			em.getTransaction().commit(); // On applique les changements
 		}
 		
 		catch (Exception ex) {
 			em.getTransaction().rollback(); // On annule les changements si Exception
 		}
+		
+		System.out.println(nouveauFournisseur.getId());
+		
+		// Pour les UPDATES
+		
+		nouveauFournisseur.setResponsable("Un autre responsable");
+		
+		// IL NOUS FAUT TOUJOURS UNE TRANSACTION
+		
+		em.getTransaction().begin();
+		
+		try {
+			nouveauFournisseur = em.merge(nouveauFournisseur);
+			em.getTransaction().commit();
+		}
+		
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+		}
+		
+		
+		
+		// Pour les DELETES
+		
+		em.getTransaction().begin();
+		
+		try {
+			// SOLUTION #1, on a déjà le fournisseur à supprimer
+//			em.remove(nouveauFournisseur); 
+
+			// SOLUTION #2, on a pas le fournisseur, on va le chercher
+//			Fournisseur fournisseurASupprimer = em.find(Fournisseur.class, 12);
+//			em.remove(fournisseurASupprimer);
+			
+			// SOLUTION #3 (pas à privilégier), on a pas le fournisseur, on va le créer
+			Fournisseur fournisseurASupprimer2 = new Fournisseur();
+			
+			fournisseurASupprimer2.setId(13);
+			fournisseurASupprimer2 = em.merge(fournisseurASupprimer2);
+			em.remove(fournisseurASupprimer2);
+			
+			em.getTransaction().commit();
+		}
+		
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+		}
+		
 	}
 }
