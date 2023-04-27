4a2dab11d915ca23b3f95693e984d16b244cb76e
Jeremy <jeremy.perrouault@ascadis.fr>
2023-04-27 12:21:07 +0200

    JOUR 1 - Cycles de vie

--- TP/SPRING-CORE/formation-spring-core-correction/src/main/java/fr/formation/Application.java
+++ TP/SPRING-CORE/formation-spring-core-correction/src/main/java/fr/formation/Application.java
@@ -9,11 +9,21 @@ public class Application {
 	public static void main(String[] args) {
 		// Créer et charger le contexte de SPRING à partir de la classe de configuration AppConfig
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//		IMusicien guitariste = context.getBean("guitariste", IMusicien.class);
+//		IMusicien guitariste2 = context.getBean("guitariste", IMusicien.class);
+//		
+//		if (guitariste == guitariste2) {
+//			System.out.println("C PAREIL");
+//		}
+//		
+//		else {
+//			System.out.println("C PAS PAREIL");
+//		}
 		
-		IMusicien guitariste = context.getBean("guitariste", IMusicien.class);
-		IMusicien pianiste = context.getBean("pianiste", IMusicien.class);
-		
-		guitariste.jouer();
-		pianiste.jouer();
+//		IMusicien pianiste = context.getBean("pianiste", IMusicien.class);
+//		
+//		guitariste.jouer();
+//		pianiste.jouer();
 	}
 }
--- TP/SPRING-CORE/formation-spring-core-correction/src/main/java/fr/formation/musique/Guitariste.java
+++ TP/SPRING-CORE/formation-spring-core-correction/src/main/java/fr/formation/musique/Guitariste.java
@@ -2,14 +2,22 @@ package fr.formation.musique;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component // Que c'est une classe "Composant de Spring"
 // > Demander à Spring de l'instancier, et de la gérer
+//@Lazy // Tant qu'on demande l'instance, Spring ne créera pas l'instance
+@Scope("prototype") // Demander à Spring une nouvelle instance à chaque demande d'instance
 public class Guitariste implements IMusicien {
 	@Autowired // Spring : si tu gère une instance de IInstrument, donne-la moi !
 	@Qualifier("guitare")
 	private IInstrument instrument;
+	
+	public Guitariste() {
+		System.out.println("CREATION D'UN GUITARISTE");
+	}
 	
 //	public void setInstrument(IInstrument instrument) {
 //		this.instrument = instrument;
