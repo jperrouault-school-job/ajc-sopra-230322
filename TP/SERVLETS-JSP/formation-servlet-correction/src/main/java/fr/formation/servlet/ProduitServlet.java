7d3a0b88c822fa0d43528b3c5890c7c3d4f7b785
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-11 15:06:06 +0200

    JOUR 1 - Correction exo Servlet Produits

---
+++ TP/SERVLETS-JSP/formation-servlet-correction/src/main/java/fr/formation/servlet/ProduitServlet.java
@@ -0,0 +1,21 @@
+package fr.formation.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/produits")
+public class ProduitServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("produit", "Un nom de produit exemple");
+		
+		this.getServletContext()
+			.getRequestDispatcher("/WEB-INF/views/produits.jsp")
+			.forward(req, resp);
+	}
+}
