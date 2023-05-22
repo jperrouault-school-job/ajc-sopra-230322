15feec59badea9477e55df754e569cda97d1d08f
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-22 11:42:56 +0200

    JOUR 1 - DEMO FORM

--- TP/SPRING-MVC/formation-spring-mvc-correction/src/main/java/fr/formation/controller/DemoController.java
+++ TP/SPRING-MVC/formation-spring-mvc-correction/src/main/java/fr/formation/controller/DemoController.java
@@ -1,13 +1,22 @@
 package fr.formation.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DemoController {
 
-	@RequestMapping("/demo")
+//	@RequestMapping("/demo")
+	@GetMapping("/demo")
 	public String demonstration() {
+		return "/WEB-INF/demo.jsp";
+	}
+	
+	@PostMapping("/demo")
+	public String postDemo() {
+		System.out.println("POST passé ici ..");
+		
 		return "/WEB-INF/demo.jsp";
 	}
 	
--- TP/SPRING-MVC/formation-spring-mvc-correction/src/main/webapp/WEB-INF/demo.jsp
+++ TP/SPRING-MVC/formation-spring-mvc-correction/src/main/webapp/WEB-INF/demo.jsp
@@ -8,5 +8,10 @@
 </head>
 <body>
 	<h1>DEMONSTRATION</h1>
+	
+	<form method="POST">
+		<input type="text" name="username" />
+		<input type="submit" value="Envoyer" />
+	</form>
 </body>
 </html>
