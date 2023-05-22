6597e645477a9ae5620e016228d4d3e68f15473c
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-22 12:58:23 +0200

    JOUR 1 - Classe Produit

---
+++ TP/SPRING-MVC/formation-spring-mvc-correction/src/main/java/fr/formation/model/Produit.java
@@ -0,0 +1,31 @@
+package fr.formation.model;
+
+public class Produit {
+	private int id;
+	private String nom;
+	private float prix;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+}
