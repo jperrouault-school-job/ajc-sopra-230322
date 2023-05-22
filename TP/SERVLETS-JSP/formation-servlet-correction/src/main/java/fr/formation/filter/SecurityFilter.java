4f93a5333eb4cfb85ceb2e47de9b72073baa9619
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-22 10:47:49 +0200

    JOUR 1 - INIT PROJET SPRING MVC

--- TP/SERVLETS-JSP/formation-servlet-correction/src/main/java/fr/formation/filter/SecurityFilter.java
+++ TP/SERVLETS-JSP/formation-servlet-correction/src/main/java/fr/formation/filter/SecurityFilter.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter("/*")
+//@WebFilter("/*")
 public class SecurityFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
