0d93d6ecc6751e326dee4e25820ead0f6b21889e
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-24 14:56:54 +0200

    JOUR 3 - UtilisateurResponse

---
+++ TP/SPRING-MVC/eshop-spring-mvc-correction/src/main/java/fr/formation/api/UtilisateurApiController.java
@@ -0,0 +1,42 @@
+package fr.formation.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.formation.api.response.UtilisateurResponse;
+import fr.formation.model.Utilisateur;
+import fr.formation.repo.IUtilisateurRepository;
+
+@RestController
+@RequestMapping("/api/utilisateur")
+public class UtilisateurApiController {
+	// DTO : Data Transform Object
+	// Le principe, c'est de créer une instance spécialement dédiée pour le transfert d'information
+	
+	@Autowired
+	private IUtilisateurRepository repoUtilisateur;
+	
+	@GetMapping
+	public List<UtilisateurResponse> findAll() {
+		List<UtilisateurResponse> response = new ArrayList<>();
+		List<Utilisateur> utilisateurs = this.repoUtilisateur.findAll();
+		
+		// Convertir les utilisateurs en UtilisateurResponse
+		for (Utilisateur utilisateur : utilisateurs) {
+			UtilisateurResponse utilisateurResponse = new UtilisateurResponse();
+
+			utilisateurResponse.setId(utilisateur.getId());
+			utilisateurResponse.setUsername(utilisateur.getUsername());
+			
+			response.add(utilisateurResponse);
+		}
+		
+		return response;
+	}
+	
+}
---
+++ TP/SPRING-MVC/eshop-spring-mvc-correction/src/main/java/fr/formation/api/response/UtilisateurResponse.java
@@ -0,0 +1,22 @@
+package fr.formation.api.response;
+
+public class UtilisateurResponse {
+	private int id;
+	private String username;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
