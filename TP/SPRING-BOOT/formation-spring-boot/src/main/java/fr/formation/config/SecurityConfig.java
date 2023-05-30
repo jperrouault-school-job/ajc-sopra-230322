4c3b9849e36ac6c9872a8796c5e66e74a8b2a580
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-30 12:28:30 +0200

    JOUR 1 - Config SecurityFilterChain

--- TP/SPRING-BOOT/formation-spring-boot/src/main/java/fr/formation/config/SecurityConfig.java
+++ TP/SPRING-BOOT/formation-spring-boot/src/main/java/fr/formation/config/SecurityConfig.java
@@ -2,12 +2,36 @@ package fr.formation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// Mise en place des authorisations
+		http.authorizeHttpRequests(authorize -> {
+			authorize.requestMatchers("/api/hello").permitAll(); // Autorisé à tout le monde
+			authorize.requestMatchers("/**").authenticated(); // Autorisé aux utilisateurs connectés
+		});
+		
+		// Méthode d'authentification par formulaire HTML
+		http.formLogin();
+		
+		// Méthode d'authentification par HTTP Basic
+//		http.httpBasic();
+		
+		// Désactiver la protection CSRF
+//		http.csrf().disable();
+		
+		return http.build();
+	}
+	
+	
 
 	@Bean
 	public UserDetailsService inMemory() {
