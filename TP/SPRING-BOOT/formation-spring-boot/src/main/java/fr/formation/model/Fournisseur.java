2fdb4cade7ed1be5802f5e78dac21e93cd4516dd
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-30 11:03:22 +0200

    JOUR 1 - INIT PROJET BOOT

---
+++ TP/SPRING-BOOT/formation-spring-boot/src/main/java/fr/formation/model/Fournisseur.java
@@ -0,0 +1,47 @@
+package fr.formation.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fournisseur")
+public class Fournisseur {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "fou_id")
+	private int id;
+
+	@Column(name = "fou_nom")
+	private String nom;
+
+	@Column(name = "fou_responsable")
+	private String responsable;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+}
