baec63daa6af4f6a865b023da9704702d55a41c2
Jeremy <jeremy.perrouault@ascadis.fr>
2023-05-31 12:09:36 +0200

    JOUR 2 - Config Positionnement JWT Filter

--- TP/SPRING-BOOT-SECURITY/formation-spring-boot-security-correction/src/main/java/fr/formation/config/SecurityConfig.java
+++ TP/SPRING-BOOT-SECURITY/formation-spring-boot-security-correction/src/main/java/fr/formation/config/SecurityConfig.java
@@ -7,22 +7,27 @@ import org.springframework.security.access.expression.method.MethodSecurityExpre
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import fr.formation.config.jwt.JwtHeaderAuthorizationFilter;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true) // Activer les annotations PrePost pour les accès
 public class SecurityConfig {
 	
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http, JwtHeaderAuthorizationFilter jwtFilter) throws Exception {
 		// Mise en place des authorisations
 		http.authorizeHttpRequests(authorize -> {
 //			authorize.requestMatchers("/api/hello").permitAll(); // Autorisé à tout le monde
@@ -37,13 +42,19 @@ public class SecurityConfig {
 		});
 		
 		// Méthode d'authentification par formulaire HTML
-//		http.formLogin();
+//		http.formLogin(Customizer.withDefaults());
 		
 		// Méthode d'authentification par HTTP Basic
-		http.httpBasic();
+		http.httpBasic(Customizer.withDefaults());
 		
 		// Désactiver la protection CSRF
-		http.csrf().disable();
+		http.csrf(c -> c.disable());
+		
+		// Positionner le filtre JWT AVANT le filter UsernamePasswordAuthenticationFilter
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		// Désactiver la session utilisateur par cookie, puisque c'est plus utilisé avec JWT
+		http.sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
 		return http.build();
 	}
