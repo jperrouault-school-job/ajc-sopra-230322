900aeda657e44494cbc0ad29867085209bac44fc
Jeremy <jeremy.perrouault@ascadis.fr>
2023-06-01 12:55:51 +0200

    JOUR 1 - Demo Objet Litteral

--- TP/ANGULAR/formation-angular/src/app/app.component.ts
+++ TP/ANGULAR/formation-angular/src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'formation-angular';
   prenom: string = "Jérémy";
+
+  personne = {
+    nom: "TOTO",
+    prenom: "Albert"
+  }
+
 }
