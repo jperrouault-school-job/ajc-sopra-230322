8dda135283d1f236ad569aa4b141f1acfb43e106
Jeremy <jeremy.perrouault@ascadis.fr>
2023-06-05 14:43:36 +0200

    JOUR 3 - DEMO OnInit

--- TP/ANGULAR/eshop-angular/src/app/form-fournisseur/form-fournisseur.component.ts
+++ TP/ANGULAR/eshop-angular/src/app/form-fournisseur/form-fournisseur.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Fournisseur } from '../fournisseur';
 
 @Component({
@@ -6,8 +6,13 @@ import { Fournisseur } from '../fournisseur';
   templateUrl: './form-fournisseur.component.html',
   styleUrls: ['./form-fournisseur.component.css']
 })
-export class FormFournisseurComponent {
-  formFournisseur: Fournisseur = new Fournisseur(0, "", "");
+export class FormFournisseurComponent implements OnInit {
+  ngOnInit(): void {
+    console.log("Chargement du composant Formulaire ...");
+    this.formFournisseur = new Fournisseur(0, "", "");
+  }
+
+  formFournisseur!: Fournisseur;
   // @Input() fournisseurs!: Fournisseur[];
   @Output() ajout = new EventEmitter<Fournisseur>();
 
