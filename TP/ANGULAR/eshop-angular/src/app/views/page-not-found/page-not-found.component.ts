d10b7f1d651c36c6227f6d2da75cd63db0947683
Jeremy <jeremy.perrouault@ascadis.fr>
2023-06-05 16:02:58 +0200

    JOUR 3 - Router PageNotFound avec redirection au bout de 2 secondes

--- TP/ANGULAR/eshop-angular/src/app/views/page-not-found/page-not-found.component.ts
+++ TP/ANGULAR/eshop-angular/src/app/views/page-not-found/page-not-found.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigate([ '/accueil' ]);
+    }, 2000);
+  }
 }
