daad4617c5a97eabc2af8b32e5037a470861235b
Jeremy <jeremy.perrouault@ascadis.fr>
2023-06-05 13:27:26 +0200

    JOUR 3 - Suppression Angular Material

--- TP/ANGULAR/eshop-angular/src/app/app.module.ts
+++ TP/ANGULAR/eshop-angular/src/app/app.module.ts
@@ -5,10 +5,7 @@ import { AppComponent } from './app.component';
 import { TableauFournisseurComponent } from './tableau-fournisseur/tableau-fournisseur.component';
 import { FormFournisseurComponent } from './form-fournisseur/form-fournisseur.component';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
+  
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,10 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    MatInputModule,
-    MatIconModule,
-    MatFormFieldModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
