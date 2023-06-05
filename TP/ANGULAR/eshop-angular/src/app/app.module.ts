f37222ba83585f84a2fa9f3e84cc5c824e29d46a
Jeremy <jeremy.perrouault@ascadis.fr>
2023-06-05 12:04:38 +0200

    JOUR 3 - Exemple intégration Material avec composants

--- TP/ANGULAR/eshop-angular/src/app/app.module.ts
+++ TP/ANGULAR/eshop-angular/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { TableauFournisseurComponent } from './tableau-fournisseur/tableau-fournisseur.component';
 import { FormFournisseurComponent } from './form-fournisseur/form-fournisseur.component';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
